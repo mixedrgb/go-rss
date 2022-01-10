23cd4e81ed89d36b40556da6d873a6d9d3147c96
toastedwedge <88474004+toastedwedge@users.noreply.github.com>
2022-01-09 18:48:54 -0600

    wip, disregard

--- main.go
+++ main.go
@@ -3,25 +3,40 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"log"
+	"net/http"
 
+	"github.com/fatih/color"
 	"github.com/mmcdole/gofeed/rss"
 )
 
 func main() {
-	file, err := os.Open("hackernews.txt")
+	url := "https://www.digitalocean.com/blog/rss/"
+	// open a test file
+	//file, err := os.Open("rss-test-hackernews.txt")
+
+	// use the http.Get() method, returns response and error
+	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
+
+	//defer file.Close()
+	defer resp.Body.Close()
+
 	// Feed Parser
 	fp := rss.Parser{}
-	feed, _ := fp.Parse(io.Reader(file))
+
+	feed, err := fp.Parse(io.Reader(resp.Body))
+	//feed, _ := fp.Parse(io.Reader(file))
+
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error with: %v\n", err)
 	}
+	c := color.New(color.FgCyan).Add(color.BgHiMagenta)
 	// feed.Items has a k:v result
 	for _, v := range feed.Items {
-		fmt.Printf(`Title: %sThread Link: %sContent: %s`, v.Title, v.Link, v.Comments)
+		fmt.Printf("\nDescription:")
+		c.Print(v.Description)
 	}
 }
